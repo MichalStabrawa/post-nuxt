4c300014e9c4e28796a8270908e14c5e2edf37f8
Michal Stabrawa <mistaaqua@gmail.com>
2019-05-12 23:20:38 +0200

    emmit event, new components Navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidnav = !displaySidnav"/>
+    <TheSidenav @close="displaySidnav = false" :show="displaySidnav"/>
     <nuxt/>
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSideNav";
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidnav: false
+    };
+  }
+};
+</script>
+
 
 <style>
 html {
