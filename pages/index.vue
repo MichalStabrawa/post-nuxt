18b41e2b2ac09c7ccccfdf0fe868124ad3fdcb7b
Michal Stabrawa <mistaaqua@gmail.com>
2019-05-12 12:56:00 +0200

    add background img

--- pages/index.vue
+++ pages/index.vue
@@ -41,6 +41,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
+  background-image: url("~assets/image/intro.jpg");
   background-position: center;
   background-size: cover;
 }
